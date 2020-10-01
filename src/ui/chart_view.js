220eae5ff0d4ad9931d9d5dd294ef99a066136bc
Natalia Sergeeva <natalia@uxcollection.com>
2020-10-01 09:05:20 -0400

    plus button

---
+++ src/ui/chart_view.js
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect } from "react"
+
+const ChartView = (ticker) => {
+
+    return(
+        <div className='new_card'>
+                <div className='pluss_icon'>+</div>
+        </div>
+    )
+}
+export default ChartView
---
+++ src/ui/new_button.js
@@ -0,0 +1,16 @@
+import React, { useRef, useEffect } from "react";
+import './styles/new_button.scss'
+
+
+import { Button } from 'react-bulma-components/dist';
+
+
+const New_button = () => {
+    
+    return (
+            <div className='new_card'>
+                <div className='pluss_icon'>+</div>
+            </div>
+    )
+}
+export default New_button
